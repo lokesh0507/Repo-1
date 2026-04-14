2097b9578dff5e0e977d74844338108def6ee615
Lokesh Immadisetty <immedisettylokesh123@gmail.com>
2026-04-14 14:54:26 +0530

    Added service-f for kafka producer topic-A

---
+++ service-f/Program.cs
@@ -0,0 +1,37 @@
+using Confluent.Kafka;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var producerConfig = new ProducerConfig
+{
+    BootstrapServers = "127.0.0.1:9092",
+    ClientId = "service-f"
+};
+
+app.MapGet("/", () => "Hello World!");
+app.MapPost("/publish", async () =>
+{
+    
+    using var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+ 
+    var eventData = new
+    {
+        EventType = "eventTypeX",
+        OrderId = Guid.NewGuid().ToString(),
+        CreatedBy = "service-f",
+        CreatedAt = DateTime.UtcNow
+    };
+ 
+    var message = new Message<Null, string>
+    {
+        Value = JsonSerializer.Serialize(eventData)
+    };
+ 
+    await producer.ProduceAsync("topic-A", message);
+ 
+    return Results.Ok("Published eventTypeX to topic-A");
+});
+
+app.Run();
